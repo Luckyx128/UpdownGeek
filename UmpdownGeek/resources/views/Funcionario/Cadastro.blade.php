b5089cdc5bfd0549ce7c63a67b46ad3734fd0dad
Luckyx128 <lucas.de.amorimarq@gmail.com>
2023-03-17 22:32:07 -0300

    Update Cadastro.blade.php

--- UmpdownGeek/resources/views/Funcionario/Cadastro.blade.php
+++ UmpdownGeek/resources/views/Funcionario/Cadastro.blade.php
@@ -6,6 +6,6 @@
 @section('content')
 <form action="/cadastro" method="Post">
     <label for="Nome">Nome:</label>
-    <input type="text" class="form-control" id="title" name="title" placeholder="Nome do Produto">
+    <input type="text" class="form-control" id="title" name="title" placeholder="Nome dos produtos">
 </form>
 @endsection
